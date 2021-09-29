e133e1534e42d6981793b727265a0610c7a2d1d7
rehanhanar7 <rehanhanar7@gmail.com>
2021-09-29 14:14:57 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  ngOnChanges() {
+    console.log('ngOnChanges 1');
+  }
+  ngOnInit() {
+    console.log('ngOnChanges 2');
+  }
+  ngDoCheck() {
+    console.log('ngOnChanges 3');
+  }
+  ngAfterContentInit() {
+    console.log('ngOnChanges 4');
+  }
+  ngAfterContentChecked() {
+    console.log('ngOnChanges 5');
+  }
+  ngAfterViewInit() {
+    console.log('ngOnChanges 6');
+  }
+  ngAfterViewChecked() {
+    console.log('ngOnChanges 7');
+  }
+}
